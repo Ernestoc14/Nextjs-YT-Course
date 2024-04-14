f36ac3fcbf9a032ff334fdc696c51442fe745bc1
Ernestoc14 <descreed34@hotmail.com>
2024-04-14 00:42:11 -0500

    Feat: Adding all posts to the page using Instagram's API

---
+++ exp/next.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "scontent-ams4-1.cdninstagram.com",
+        port: "",
+      },
+      {
+        protocol: "https",
+        hostname: "scontent-mad1-1.cdninstagram.com",
+      },
+      {
+        protocol: "https",
+        hostname: "scontent-mad2-1.cdninstagram.com",
+      }
+    ]
+  }
+}
